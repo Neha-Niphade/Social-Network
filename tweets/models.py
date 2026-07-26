dcb7cecaf292fe647ffa985e123eaf301b301683
Your Name <you@example.com>
2026-07-27 00:10:50 +0530

    Add Tweet model and register in Django admin

--- tweets/models.py
+++ tweets/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Tweet(models.Model):
+    text = models.TextField()
+    photo = models.ImageField(upload_to='photos/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text[:30]
